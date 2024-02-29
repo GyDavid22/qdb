ea799582d314c5ccb2286b32b8376b0859b27a82
David Gyenes <gyenesdavid@gmail.com>
2024-02-29 15:10:43 +0100

    Easier deployment

--- qdb_web/src/constants.ts
+++ qdb_web/src/constants.ts
@@ -1,3 +1,12 @@
+import { environment } from "./environments/environment";
+
 export class Constants {
-    public static readonly WEBPAGE_URL = "http://localhost:8080/"
+    public static WEBPAGE_URL: string = "";
+    static {
+        if (environment.production) {
+            Constants.WEBPAGE_URL = "http://157.230.28.235/";
+        } else {
+            Constants.WEBPAGE_URL = "http://localhost:8080/";
+        }
+    }
 }
