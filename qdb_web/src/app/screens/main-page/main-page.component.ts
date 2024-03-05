1da027c3f7588d227fe88904a972330622b872b8
David Gyenes <gyenesdavid@gmail.com>
2024-03-05 15:19:03 +0100

    Revert

--- qdb_web/src/app/screens/main-page/main-page.component.ts
+++ qdb_web/src/app/screens/main-page/main-page.component.ts
@@ -1,13 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { QueryService } from '../../services/query.service';
 import { TagResponse } from '../../entities/TagResponse';
+import { QuestionCardComponent } from '../common-elements/question-card/question-card.component';
 import { TagsBoxComponent } from '../common-elements/tags-box/tags-box.component';
-import { QuestionsPagedComponent } from '../common-elements/questions-paged/questions-paged.component';
+import { QuestionMetadataList } from '../../entities/QuestionMetadataList';
+import { PaginatingComponent } from '../common-elements/paginating/paginating.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [TagsBoxComponent, QuestionsPagedComponent],
+  imports: [NgFor, QuestionCardComponent, TagsBoxComponent, PaginatingComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
@@ -19,10 +22,24 @@ export class MainPageComponent {
   public get tagsWithCount(): TagResponse[] {
     return this._tagsWithCount;
   }
+  public questions: QuestionMetadataList | undefined;
+  private pageIndex: number = 0;
+  private _pageSize: number | undefined = PaginatingComponent.DEFAULT_PAGESIZE;
+  public set pageSize(value: number | undefined) {
+    this._pageSize = value;
+    this.performQuery();
+  }
 
   constructor(private qService: QueryService) {
     this.qService.tagsWithCounts().then((value) => {
       this.tagsWithCount = value;
+    });
+    this.performQuery();
+  }
+
+  private performQuery() {
+    this.qService.getQuestionMetadataList(this.pageIndex, this._pageSize).then((value) => {
+      this.questions = value;
     });
   }
 }
