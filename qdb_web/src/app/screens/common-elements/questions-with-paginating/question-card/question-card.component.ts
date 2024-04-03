58fb80715e9b10bd1b5fb2d8545798b24a978a0d
David Gyenes <gyenesdavid@gmail.com>
2024-04-03 12:35:55 +0200

    Highlight search term in question card

--- qdb_web/src/app/screens/common-elements/questions-with-paginating/question-card/question-card.component.ts
+++ qdb_web/src/app/screens/common-elements/questions-with-paginating/question-card/question-card.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, SecurityContext } from '@angular/core';
-import { QuestionMetadata } from '../../../../entities/QuestionMetadata';
 import { NgFor, NgIf } from '@angular/common';
+import { Component, Input, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
+import { marked } from 'marked';
+import { Constants } from '../../../../../constants';
+import { QuestionMetadata } from '../../../../entities/QuestionMetadata';
 import { QueryService } from '../../../../services/query.service';
 import { TagBadgesComponent } from '../../tags-box/tag-badges/tag-badges.component';
-import { marked } from 'marked';
-import { DomSanitizer } from '@angular/platform-browser';
-import { Constants } from '../../../../../constants';
 
 @Component({
   selector: 'app-question-card',
@@ -22,6 +22,7 @@ export class QuestionCardComponent {
   }
   @Input() public set question(value: QuestionMetadata) {
     this._question = value;
+    this.title = value.title;
   }
   @Input() public selfNumber: number | undefined;
   @Input() public set titleOnly(val: boolean | undefined) {
@@ -32,6 +33,16 @@ export class QuestionCardComponent {
       });
     }
   }
+  @Input() public set highlight(val: string | undefined) {
+    if (this._highlight === undefined && val !== undefined) {
+      this._highlight = val;
+      this.title = this.replaceWithHighlight(this.title);
+    }
+  }
+  public get highlight(): string | undefined {
+    return this._highlight;
+  }
+  private _highlight: string | undefined;
   public get titleOnly(): boolean | undefined {
     return this._titleOnly;
   }
@@ -47,11 +58,36 @@ export class QuestionCardComponent {
       let imagename = i.substring(i.lastIndexOf("/") + 1);
       this._body = this._body.replace(imagename, `${Constants.WEBPAGE_URL}java/api/image/${imagename}`);
     }
+    if (this.highlight !== undefined) {
+      this._body = this.replaceWithHighlight(this._body);
+    }
     this._body = this.sanitizer.sanitize(SecurityContext.HTML, marked.parse(this._body));
   }
   public get body(): string | null {
     return this._body;
   }
+  private _title: string = "";
+  public get title(): string {
+    return this._title;
+  }
+  public set title(val: string) {
+    this._title = this.sanitizer.sanitize(SecurityContext.HTML, val)!;
+  }
 
   constructor(private qService: QueryService, private sanitizer: DomSanitizer) { }
+
+  private replaceWithHighlight(text: string): string {
+    if (this.highlight !== undefined) {
+      let lowercase = text.toLowerCase();
+      let highlightLovercase = this.highlight.toLowerCase();
+      let index = lowercase.indexOf(highlightLovercase, 0);
+      while (index !== -1) {
+        let replaceValue = `<span class="mark">${text.substring(index, index + this.highlight.length)}</span>`;
+        text = text.substring(0, index) + replaceValue + text.substring(index + this.highlight.length);
+        lowercase = text.toLowerCase();
+        index = lowercase.indexOf(highlightLovercase, index + replaceValue.length);
+      }
+    }
+    return text;
+  }
 }
